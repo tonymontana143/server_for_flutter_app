544751836b8bde330598481ad0948bb0299cfd38
iliias <kenes_2005@mail.ru>
2024-05-23 17:13:12 +0500

    done with staff

--- services/staff.service.js
+++ services/staff.service.js
@@ -1,41 +1,60 @@
-const StaffModel = require("../model/staff_model");
+const Staff = require('../model/staff_model');
 
 class StaffService {
-
-    static async createStaff(
-            id,
-            name,
-            surname,
-            imgUrl,
-            position,
-            description
-    ) {
-
-        const createStaff = new StaffModel(
-            {
-            id,
-            name,
-            surname,
-            imgUrl,
-            position,
-            description
-            }
-        );
-
-        return await createStaff.save();
-
+  static async createStaff(name, surname, imgUrl, role, description) {
+    try {
+      const staff = new Staff({ name, surname, imgUrl, role, description });
+      await staff.save();
+      return staff;
+    } catch (error) {
+      throw new Error('Error creating staff: ' + error.message);
     }
+  }
 
-    static async getStaffData(id) {
-        const staffData = await StaffModel.find({
-            id
-        });
-
-
-
-        return staffData;
+  static async getStaff() {
+    try {
+      const staffs = await Staff.find();
+      return staffs;
+    } catch (error) {
+      throw new Error('Error fetching staffs: ' + error.message);
     }
+  }
 
+  static async getStaffById(id) {
+    try {
+      const staff = await Staff.findById(id);
+      if (!staff) {
+        throw new Error('Staff not found');
+      }
+      return staff;
+    } catch (error) {
+      throw new Error('Error fetching staff: ' + error.message);
+    }
+  }
+
+  static async updateStaff(id, name, surname, imgUrl, role, description) {
+    try {
+      const staff = await Staff.findByIdAndUpdate(id, { name, surname, imgUrl, role, description }, { new: true });
+      if (!staff) {
+        throw new Error('Staff not found');
+      }
+      return staff;
+    } catch (error) {
+      throw new Error('Error updating staff: ' + error.message);
+    }
+  }
+
+  static async deleteStaff(id) {
+    try {
+      const staff = await Staff.findByIdAndDelete(id);
+      if (!staff) {
+        throw new Error('Staff not found');
+      }
+      return { message: 'Staff deleted successfully' };
+    } catch (error) {
+      throw new Error('Error deleting staff: ' + error.message);
+    }
+  }
 }
 
 module.exports = StaffService;
